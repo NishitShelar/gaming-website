2b5936344d6e71b21e3c6441c86f80eadfe7bd26
NishitShelar <nishitsshelar@gmail.com>
2025-09-08 13:03:49 +0530

    complete fully responsive ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Characters from "./components/Characters"
 import Arena from "./components/Arena"
+import Footer from "./components/Footer"
 const App = () => {
   return (
     <div className=''>
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <Characters/>
       <Arena/>
+      <Footer/>
     </div>
   )
 }
